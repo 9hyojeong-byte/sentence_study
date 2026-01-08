846c6265ae0b7006d8987c56d4afa68077d6cd22
9hyojeong-byte <9.hyojeong@gmail.com>
2026-01-08 19:21:31 +0900

    수정 버전

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // 상대 경로 기반으로 빌드하여 어디서든 잘 보이게 합니다.
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false
+  },
   server: {
-    port: 3000
+    port: 3000,
+    strictPort: true,
+    host: true
   }
 });
